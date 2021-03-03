bfa6901c9936b596e229f90caec187b63a8d7786
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 11:11:15 +0100

    Updated calculator

--- calculator.php
+++ calculator.php
@@ -21,6 +21,7 @@
    <!-- HOME ---------------------------------------->
 
     <section id="imprint" class="container" class="vertical-center">
+        <div id="calc">
         <form>
         <textarea id="eingabe" readonly></textarea>
         </br>
@@ -37,14 +38,15 @@
         <button type="button" onclick="one()" class="numberbutton" id="1">1</button>
         <button type="button" onclick="two()" class="numberbutton" id="2">2</button>
         <button type="button" onclick="three()" class="numberbutton" id="3">3</button>
-        <button type="button" onclick="plus()" class="numberbutton" id="plus">+</button> 
+        <button type="button" onclick="plusb()" class="numberbutton" id="plus">+</button> 
         </br>
-        <button type="button" onclick="zero()" class="zerobutton" id="0">0</button>
-        <button type="button" onclick="coma()" class="comabutton" id="coma">,</button>
-        <button type="button" onclick="equals()" class="numberbutton" id="equals">=</button>
+        <button type="button" onclick="zero()" class="numberbutton" id="0">0</button>
+        <button type="button" onclick="pib()" class="numberbutton" id="pi">Pi</button>
+        <button type="button" onclick="coma()" class="numberbutton" id="coma">,</button>
+        <button type="button" onclick="equalsb()" class="numberbutton" id="equals">=</button>
 
         </form>
-
+        </div>
     </section>
 
     <script>
@@ -63,6 +65,50 @@
         function four() {
             var t1 = document.getElementById("eingabe").value
             document.getElementById("eingabe").value = t1 + "4";
+        }
+        function five() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "5";
+        }
+        function six() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "6";   
+        }
+        function seven() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "7";
+        }
+        function eight() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "8";
+        }
+        function nine() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "9";
+        }
+        function zero() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "0";
+        }
+        function CE() {
+            document.getElementById("eingabe").value = "";
+        }
+        function back() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1.substring(0, t1.length - 1);
+        }
+        function equalsb() {
+            var t1 = document.getElementById("eingabe").value  
+            var t2 = t1.replace("𝜋", "Math.PI"); 
+            document.getElementById("eingabe").value = eval(t2);
+        }
+        function plusb() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "+";
+        }
+        function pib() {
+            var t1 = document.getElementById("eingabe").value
+            document.getElementById("eingabe").value = t1 + "𝜋";
         }
     </script>
 
